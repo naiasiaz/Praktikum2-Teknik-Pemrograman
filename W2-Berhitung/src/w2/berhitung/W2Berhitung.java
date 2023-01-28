88d4e72bd14916707da5b38bc4de1759ca3bd805
naiasiaz <naiasitiazzahra27@gmail.com>
2023-01-28 15:06:44 +0700

    Soal 3

---
+++ W2-Berhitung/src/w2/berhitung/W2Berhitung.java
@@ -0,0 +1,62 @@
+package w2.berhitung;
+
+import java.util.Scanner; // Memanggil scanner untuk melakukan inputan dari keyboard
+
+/** 
+ * <h1> Berhitung </h1>
+ * 
+ * Program ini meminta user untuk menginputkan angka dan jenis operasi, lalu program akan mnjalankan perintah yang diinputkan
+ * user untuk perhitungan matematika seperti pertambahan, pengurangan, perkalian, pembagian, serta modulus atau hasil bagi
+ * 
+ * @author Naia Siti Az-zahra
+ * @version 1.0
+ * @since 2023-01-28
+ */
+
+public class W2Berhitung {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Deklarasi Variabel
+        int angka1, angka2, hasil;
+        char operator;
+        
+        // Membuat Scanner Baru
+        Scanner keyboard = new Scanner(System.in);
+        
+        // Algoritma
+        angka1 = keyboard.nextInt();
+        operator = keyboard.next().charAt(0);
+        angka2 = keyboard.nextInt();
+        
+        if((angka1 >= 1 && angka1 <= 1000) && (angka2 >= 1 && angka2 <= 1000)) {
+            switch (operator) {
+                case '+' -> { // Jika operator "+" maka akan dilakukan pertambahan angka1 + angka2
+                    hasil = angka1 + angka2;
+                    System.out.println(hasil);
+                }
+                case '-' -> { // Jika operator "-" maka akan dilakukan pengurangan angka1 - angka2
+                    hasil = angka1 - angka2;
+                    System.out.println(hasil);
+                }
+                case '*' -> { // Jika operator "*" maka akan dilakukan perkalian angka1 * angka2
+                    hasil = angka1 * angka2;
+                    System.out.println(hasil);
+                }
+                case '/' -> { // Jika operator "/" maka akan dilakukan pembagian angka1 / angka2
+                    hasil = angka1 / angka2;
+                    System.out.println(hasil);
+                }
+                case '%' -> { // Jika operator "%" maka akan dilakukan hasil bagi angka1 % angka2
+                    hasil = angka1 % angka2;
+                    System.out.println(hasil);
+                }
+                default -> {
+                }
+            }
+        }
+    }
+    
+}
